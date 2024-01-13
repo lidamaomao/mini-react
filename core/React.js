3b612d3e147d7e9496dd7f21d9ccb5275fc85126
lidamao <916929789@qq.com>
2024-01-13 19:53:53 +0800

    模仿React架构

---
+++ core/React.js
@@ -0,0 +1,59 @@
+function createTextNode(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) => {
+        return typeof child === "string" ? createTextNode(child) : child;
+      }),
+    },
+  };
+}
+
+// const textEl = createTextNode("app");
+// const App = createElement("div", { id: "app" }, textEl);
+
+// const dom = document.createElement(App.type);
+// dom.id = App.props.id;
+// document.querySelector("#root").append(dom);
+
+// const textNode = document.createTextNode("");
+// textNode.nodeValue = textEl.props.nodeValue;
+// dom.appendChild(textNode);
+
+function render(el, container) {
+  const dom =
+    el.type === "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(el.type);
+
+  Object.keys(el.props).forEach((key) => {
+    if (key !== "children") {
+      dom[key] = el.props[key];
+    }
+  });
+
+  const children = el.props.children;
+  children.forEach((child) => {
+    render(child, dom);
+  });
+
+  container.append(dom);
+}
+
+const React = {
+  createElement,
+  render,
+};
+
+export default React;
--- main.js
+++ main.js
@@ -26,71 +26,10 @@
 //   },
 // };
 
-function createTextNode(text) {
-  return {
-    type: "TEXT_ELEMENT",
-    props: {
-      nodeValue: text,
-      children: [],
-    },
-  };
-}
+import ReactDom from "./core/ReactDom.js";
+import React from "./core/React.js";
 
-function createElement(type, props, ...children) {
-  return {
-    type,
-    props: {
-      ...props,
-      children: children.map((child) => {
-        return typeof child === "string" ? createTextNode(child) : child;
-      }),
-    },
-  };
-}
-
-// const textEl = createTextNode("app");
-// const App = createElement("div", { id: "app" }, textEl);
-
-// const dom = document.createElement(App.type);
-// dom.id = App.props.id;
-// document.querySelector("#root").append(dom);
-
-// const textNode = document.createTextNode("");
-// textNode.nodeValue = textEl.props.nodeValue;
-// dom.appendChild(textNode);
-
-function render(el, container) {
-  const dom =
-    el.type === "TEXT_ELEMENT"
-      ? document.createTextNode("")
-      : document.createElement(el.type);
-
-  Object.keys(el.props).forEach((key) => {
-    if (key !== "children") {
-      dom[key] = el.props[key];
-    }
-  });
-
-  const children = el.props.children;
-  children.forEach((child) => {
-    render(child, dom);
-  });
-
-  container.append(dom);
-}
-
-const textEl = createTextNode("app");
-const App = createElement("div", { id: "app" }, "hello ", "mini-react");
+const App = React.createElement("div", { id: "app" }, "hello ", "mini-react");
 // render(App, document.getElementById("root"));
-
-const ReactDom = {
-  createRoot(container) {
-    return {
-      render(App) {
-        render(App, container);
-      },
-    };
-  },
-};
 
 ReactDom.createRoot(document.getElementById("root")).render(App);

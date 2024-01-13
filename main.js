55d81ca6ae73d71ec5a674f83ec2f7ffa56fe547
lidamao <916929789@qq.com>
2024-01-13 19:45:45 +0800

    mini-react功能初步实现

--- main.js
+++ main.js
@@ -1,8 +1,96 @@
 // v1
-const dom = document.createElement("div");
-dom.id = "app";
-document.querySelector("#root").append(dom);
+// const dom = document.createElement("div");
+// dom.id = "app";
+// document.querySelector("#root").append(dom);
 
-const textNode = document.createTextNode("");
-textNode.nodeValue = "app";
-dom.appendChild(textNode);
+// const textNode = document.createTextNode("");
+// textNode.nodeValue = "app";
+// dom.appendChild(textNode);
+
+// v2 react->vdom-> js object
+
+// object -> type props children
+// const textEl = {
+//   type: "TEXT_ELEMENT",
+//   props: {
+//     nodeValue: "app",
+//     children: [],
+//   },
+// };
+
+// const el = {
+//   type: "div",
+//   props: {
+//     id: "app",
+//     children: [textEl],
+//   },
+// };
+
+function createTextNode(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) => {
+        return typeof child === "string" ? createTextNode(child) : child;
+      }),
+    },
+  };
+}
+
+// const textEl = createTextNode("app");
+// const App = createElement("div", { id: "app" }, textEl);
+
+// const dom = document.createElement(App.type);
+// dom.id = App.props.id;
+// document.querySelector("#root").append(dom);
+
+// const textNode = document.createTextNode("");
+// textNode.nodeValue = textEl.props.nodeValue;
+// dom.appendChild(textNode);
+
+function render(el, container) {
+  const dom =
+    el.type === "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(el.type);
+
+  Object.keys(el.props).forEach((key) => {
+    if (key !== "children") {
+      dom[key] = el.props[key];
+    }
+  });
+
+  const children = el.props.children;
+  children.forEach((child) => {
+    render(child, dom);
+  });
+
+  container.append(dom);
+}
+
+const textEl = createTextNode("app");
+const App = createElement("div", { id: "app" }, "hello ", "mini-react");
+// render(App, document.getElementById("root"));
+
+const ReactDom = {
+  createRoot(container) {
+    return {
+      render(App) {
+        render(App, container);
+      },
+    };
+  },
+};
+
+ReactDom.createRoot(document.getElementById("root")).render(App);
